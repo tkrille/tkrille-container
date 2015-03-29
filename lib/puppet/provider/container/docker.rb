734d94b4283d71bdb6c54a98d596ba64e0ae82d3
Thomas Krille <tkrille@gmail.com>
2015-03-30 00:14:32 +0200

    first working version

---
+++ lib/puppet/provider/container/docker.rb
@@ -0,0 +1,119 @@
+# Copyright 2015 Thomas Krille
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+Puppet::Type.type(:container).provide(:docker) do
+  desc 'Docker support for running containers'
+
+  FQIN_PATTERN = /\A([\w\-\.]+?(:[\d]+?)?\/)?([\w\-\.\+]+\/)?([\w\-\.]+)(:[\w\-\.]+)?\z/
+
+  confine :kernel => :linux
+  commands :docker => 'docker'
+
+  def initialize(value ={})
+    super(value)
+    @property_changed = false
+  end
+
+  def self.instances
+    container_lines = docker('ps', '--all', '--no-trunc').split(/\n/)
+    container_lines.shift
+    container_lines.map! { |it| it.split }
+
+    container_ids = container_lines.map { |it| it[0] }
+    container_ids_to_image = Hash[container_lines.map { |it| [it[0], it[1]] }]
+
+    return [] if container_ids.empty?
+
+    JSON.parse(docker('inspect', container_ids)).map do |container|
+
+      name = container['Name'].split('/').last
+      image = container_ids_to_image[container['Id']]
+      env = Hash[container['Config']['Env'].collect do |it|
+                   parts = it.split '=', 2
+                   [parts.first, parts.last]
+                 end]
+
+      new(:name => name,
+          :ensure => :present,
+          :image => image,
+          :env => env)
+    end
+  end
+
+  def self.prefetch(resources)
+    containers = instances
+    resources.keys.each do |name|
+
+      provider = containers.find { |it| it.name == name }
+
+      if provider
+        resources[name].provider = provider
+      end
+    end
+  end
+
+  mk_resource_methods
+
+  def create
+    docker('run', '-d', '--name', @resource[:name], @resource[:image])
+  end
+
+  def destroy
+    docker('stop', @resource[:name])
+    docker('rm', '-f', @resource[:name])
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  def validate
+    # TODO: check invariants
+  end
+
+  def image=(fqin)
+    @property_changed = true
+  end
+
+  def image_validate(fqin)
+    if (fqin =~ FQIN_PATTERN).nil?
+      fail("invalid value '#{fqin}' for 'image'")
+    end
+  end
+
+  def image_munge(fqin)
+    fqin.scan FQIN_PATTERN do |match|
+      Puppet.debug("match = #{match}")
+      if match[4].nil?
+        return "#{fqin}:latest"
+      end
+    end
+
+    fqin
+  end
+
+  def env=(env_hash)
+    @property_changed = true
+  end
+
+  def flush
+    if @property_changed
+      Puppet.debug("flush: renewing container #{@resource[:name]}")
+      destroy
+      create
+    end
+
+    @property_hash = resource.to_hash
+  end
+end
---
+++ lib/puppet/type/container.rb
@@ -0,0 +1,70 @@
+# Copyright 2015 Thomas Krille
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+Puppet::Type.newtype(:container) do
+  @doc = 'Create a new container and optionally start it.'
+
+  ensurable
+  # ensurable do
+  #   desc 'What state the container should be in.'
+  #
+  #   newvalue(:started) do
+  #   end
+  #
+  #   aliasvalue(:present, :started)
+  #
+  #   newvalue(:stopped) do
+  #   end
+  #
+  #   newvalue(:removed) do
+  #   end
+  #
+  #   aliasvalue(:absent, :removed)
+  #
+  #   defaultto :started
+  # end
+
+  newparam(:name, :namevar => true) do
+    desc 'The name of the container to manage'
+  end
+
+  newproperty(:image) do
+    desc 'The fully qualified image name to create the container from. Required.'
+
+    validate do |fqin|
+      fail('\'image\' must not be empty') if fqin.to_s.empty?
+      provider.image_validate fqin.to_s
+    end
+
+    munge do |fqin|
+      provider.image_munge fqin.to_s
+    end
+  end
+
+  newproperty(:env) do
+    desc 'Environment variables as a Hash'
+
+    validate do |value|
+      fail('Parameter \'env\' must be a Hash') unless value.is_a?(Hash)
+    end
+
+    # TODO: use 'insync?'
+  end
+
+  validate do
+    fail('\'image\' is required when ensure is \'present\'') if self[:ensure] == :present and self[:image].nil?
+    provider.validate
+  end
+
+end
